9cf884aec6077729e41a823e2b5067f1e06c8aec
Kerry McAdams <58492561+klmcadams@users.noreply.github.com>
2024-06-27 14:43:02 -0400

    fix: update API documentation (#26)
    Co-authored-by: pyansys-ci-bot <pyansys.github.bot@ansys.com>

---
+++ doc/source/_templates/autoapi/index.rst
@@ -0,0 +1,14 @@
+.. vale off
+API reference
+=============
+
+This documentation lists all supported APIs used in Ansys Mechanical. It provides brief
+descriptions of the objects, methods, and properties for all namespaces.
+
+.. toctree::
+   :titlesonly:
+   :maxdepth: 3
+
+   <span class="nf nf-md-package"></span>Mechanical 2024 R1</api/ansys/mechanical/stubs/v241/index>
+
+.. vale on
--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,147 +1,136 @@
+"""Sphinx documentation configuration file."""
+
 # Configuration file for the Sphinx documentation builder.
 #
-# For the full list of built-in configuration values, see the documentation:
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-from datetime import datetime
-import importlib
-import os
-from pathlib import Path
+# -- Path setup --------------------------------------------------------------
 
-from ansys_sphinx_theme import (
-    ansys_favicon,
-    get_autoapi_templates_dir_relative_path,
-    get_version_match,
-    pyansys_logo_black,
-)
-from sphinx.builders.latex import LaTeXBuilder
+from datetime import datetime
+import os
+
+from ansys_sphinx_theme import ansys_favicon, get_version_match, pyansys_logo_black
 
 from ansys.mechanical.stubs import __version__
 
-LaTeXBuilder.supported_image_types = ["image/png", "image/pdf", "image/svg+xml"]
-
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-# Project information
-project = "ansys-mechanical-stubs"
+project = "ansys.mechanical.stubs"
 copyright = f"(c) {datetime.now().year} ANSYS, Inc. All rights reserved"
-author = "ANSYS, Inc."
+author = "ANSYS Inc."
 release = version = __version__
 cname = os.getenv("DOCUMENTATION_CNAME", default="scripting.mechanical.docs.pyansys.com")
-switcher_version = get_version_match(__version__)
 
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 # Sphinx extensions
 extensions = [
-    "autoapi.extension",
+    "ansys_sphinx_theme.extension.autoapi",
+    "jupyter_sphinx",
+    "notfound.extension",
     "numpydoc",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.coverage",
+    "sphinx.ext.doctest",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
     "sphinx_autodoc_typehints",
     "sphinx_copybutton",
     "sphinx_design",
-    "sphinx.ext.autodoc",
-    "sphinx.ext.autosummary",
-    "sphinx.ext.intersphinx",
-    "sphinx_markdown_builder",
 ]
-
-markdown_anchor_sections = True
-markdown_anchor_signatures = True
-
-exclude_patterns = ["_autoapi_templates", "_build", "Thumbs.db", ".DS_Store"]
-
-# Configuration for Sphinx autoapi
-autoapi_type = "python"
-autoapi_dirs = ["../../src/ansys"]
-autoapi_root = "api"
-autoapi_options = [
-    "members",
-    "undoc-members",
-    "show-inheritance",
-    "show-module-summary",
-    "special-members",
-]
-autoapi_template_dir = get_autoapi_templates_dir_relative_path(Path(__file__))
-suppress_warnings = ["autoapi.python_import_resolution"]
-autoapi_python_use_implicit_namespaces = True
-autoapi_keep_files = True
-autoapi_render_in_single_page = ["class", "enum", "exception"]
 
 # Intersphinx mapping
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy/", None),
     "numpy": ("https://numpy.org/devdocs", None),
     "matplotlib": ("https://matplotlib.org/stable", None),
-    "imageio": ("https://imageio.readthedocs.io/en/stable", None),
-    "pandas": ("https://pandas.pydata.org/pandas-docs/stable", None),
-    "pytest": ("https://docs.pytest.org/en/stable", None),
+    "grpc": ("https://grpc.github.io/grpc/python/", None),
+    "pypim": ("https://pypim.docs.pyansys.com/version/dev/", None),
 }
 
-# Numpydoc config
+suppress_warnings = ["label.*", "autoapi.python_import_resolution", "design.grid", "config.cache"]
+# supress_warnings = ["ref.option"]
+
+
+# numpydoc configuration
 numpydoc_use_plots = True
-numpydoc_show_class_members = False  # we take care of autosummary on our own
+numpydoc_show_class_members = False
 numpydoc_xref_param_type = True
 numpydoc_validate = True
 numpydoc_validation_checks = {
-    # general
     "GL06",  # Found unknown section
     "GL07",  # Sections are in the wrong order.
     # "GL08",  # The object does not have a docstring
     "GL09",  # Deprecation warning should precede extended summary
     "GL10",  # reST directives {directives} must be followed by two colons
-    # Summary
     "SS01",  # No summary found
     "SS02",  # Summary does not start with a capital letter
-    "SS03",  # Summary does not end with a period
+    # "SS03", # Summary does not end with a period
     "SS04",  # Summary contains heading whitespaces
-    "SS05",  # Summary must start with infinitive verb, not third person
-    # Parameters
-    "PR10",  # Parameter "{param_name}" requires a space before the colon '
-    # separating the parameter name and type",
+    # "SS05", # Summary must start with infinitive verb, not third person
+    "RT02",  # The first line of the Returns section should contain only the
+    # type, unless multiple values are being returned"
 }
 
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+numpydoc_validation_exclude = {  # set of regex
+    # grpc files
+    r"\.*pb2\.*",
+}
 
 # Favicon
 html_favicon = ansys_favicon
 
+# static path
+templates_path = ["_templates"]
 # The suffix(es) of source filenames.
 source_suffix = ".rst"
 
 # The master toctree document.
 master_doc = "index"
 
-# The name of the Pygments (syntax highlighting) style to use.
-pygments_style = "sphinx"
-
-# The language for content autogenerated by Sphinx_PyAEDT. Refer to documentation
+# The language for content autogenerated by Sphinx. Refer to documentation
 # for a list of supported languages.
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
 language = "en"
 
-# Select desired logo, theme, and declare the html title
-html_logo = pyansys_logo_black
-html_theme = "ansys_sphinx_theme"
-html_short_title = html_title = "Mechanical API Documentation"
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+    "links.rst",
+]
 
-# specify the location of your github repo
+# Copy button customization ---------------------------------------------------
+# exclude traditional Python prompts from the copied code
+copybutton_prompt_text = r">>> ?|\.\.\. "
+copybutton_prompt_is_regexp = True
+
+# -- Options for HTML output -------------------------------------------------
+html_short_title = html_title = "PyMechanical Stubs"
+html_theme = "ansys_sphinx_theme"
+html_logo = pyansys_logo_black
 html_context = {
-    "github_user": "ansys",
+    "github_user": "pyansys",
     "github_repo": "pymechanical-stubs",
     "github_version": "main",
     "doc_path": "doc/source",
 }
 html_theme_options = {
-    "navigation_depth": 10,
     "switcher": {
         "json_url": f"https://{cname}/versions.json",
-        "version_match": switcher_version,
+        "version_match": get_version_match(version),
     },
     "check_switcher": False,
     "github_url": "https://github.com/ansys/pymechanical-stubs",
@@ -149,12 +138,40 @@ html_theme_options = {
     "show_breadcrumbs": True,
     "collapse_navigation": True,
     "use_edit_page_button": True,
+    "header_links_before_dropdown": 4,  # number of links before the dropdown menu
     "additional_breadcrumbs": [
         ("PyAnsys", "https://docs.pyansys.com/"),
-        ("PyMechanical", "https://mechanical.docs.pyansys.com/"),
-        (
-            "Mechanical Scripting",
-            "https://mechanical.docs.pyansys.com/version/stable/user_guide_scripting/index.html",
-        ),
     ],
+    "icon_links": [
+        {
+            "name": "Support",
+            "url": "https://github.com/ansys/pymechanical-stubs/discussions",
+            "icon": "fa fa-comment fa-fw",
+        },
+    ],
+    "use_meilisearch": {
+        "api_key": os.getenv("MEILISEARCH_PUBLIC_API_KEY", ""),
+        "index_uids": {
+            f"pymechanical-stubs-v{get_version_match(version).replace('.', '-')}": "PyMechanical Stubs",
+        },
+    },
+    "ansys_sphinx_theme_autoapi": {"project": project, "templates": "_templates/autoapi"},
+    "navigation_depth": 10,
 }
+
+markdown_anchor_sections = True
+markdown_anchor_signatures = True
+
+# -- Linkcheck config --------------------------------------------------------
+
+linkcheck_ignore = []
+
+linkcheck_anchors = False
+
+# If we are on a release, we have to ignore the "release" URLs, since it is not
+# available until the release is published.
+switcher_version = get_version_match(version)
+if switcher_version != "dev":
+    linkcheck_ignore.append(
+        f"https://github.com/ansys/pymechanical-stubs/releases/tag/v{__version__}"
+    )
