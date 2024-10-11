4d1b46b1fca8848e996d11923eaba31265e1c1b5
klmcadams <58492561+klmcadams@users.noreply.github.com>
2024-10-11 14:29:35 -0400

    workflow testing

--- stub_generator/create_files.py
+++ stub_generator/create_files.py
@@ -43,7 +43,7 @@ def get_version():
     int
         The version of the Ansys install set in the AWP_ROOTDV_DEV environment variable.
     """
-    install_dir = os.environ["AWP_ROOT242"]  # ["AWP_ROOTDV_DEV"]
+    install_dir = os.environ["AWP_ROOTDV_DEV"]
     version = int(install_dir[-3:])
 
     return install_dir, version
@@ -206,9 +206,9 @@ def main():
 
     # Assembly files to read from the Ansys Mechanical install.
     assemblies = [
-        "Ansys.Mechanical.DataModel",
+        # "Ansys.Mechanical.DataModel",
         "Ansys.Mechanical.Interfaces",
-        "Ansys.ACT.WB1",
+        # "Ansys.ACT.WB1",
     ]
 
     resolve()
