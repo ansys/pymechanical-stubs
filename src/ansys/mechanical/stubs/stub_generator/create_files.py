3410b46ce897ed1aef2efd35821720161098aa10
klmcadams <58492561+klmcadams@users.noreply.github.com>
2024-12-18 10:36:58 -0500

    point DataModelObject to IDataModelObject

--- src/ansys/mechanical/stubs/stub_generator/create_files.py
+++ src/ansys/mechanical/stubs/stub_generator/create_files.py
@@ -32,7 +32,11 @@ import generate_content
 
 import System  # isort: skip
 
-ACCEPTED_TYPES = {"Ansys.Core.Units.Quantity", "Ansys.ACT.Interfaces.Common"}
+ACCEPTED_TYPES = {
+    "Ansys.Core.Units.Quantity",
+    "Ansys.ACT.Interfaces.Common",
+    "Ansys.Mechanical.DataModel.Interfaces.IDataModelObject",
+}
 
 
 def get_version():
@@ -199,7 +203,15 @@ def make(base_dir, outdir, assemblies, str_version):
                         contents = contents.replace(
                             "import typing", f"import typing\n{''.join(import_statements)}"
                         )
+
                         f.write(contents)
+
+                        datamodel_interfaces = (
+                            pathlib.Path("Ansys") / "Mechanical" / "DataModel" / "Interfaces"
+                        )
+                        if str(datamodel_interfaces) in str(init_path):
+                            f.write("class DataModelObject(IDataModelObject):\n")
+                            f.write("    pass\n")
 
     print("Done processing all mechanical stubs.")
 
