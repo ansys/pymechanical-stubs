21d8b9d860574a5738392fc57a30ecda16d60a68
Kerry McAdams <kerry.mcadams@ansys.com>
2025-07-21 13:17:33 -0400

    adjust script and label.yml

--- scripts/01-clean-id.py
+++ scripts/01-clean-id.py
@@ -51,7 +51,7 @@ def remove_links_from_markdown_files(directory_path):
                         # Delete the <!-- vale on --> comment
                         joined_lines = joined_lines.replace("<!-- vale on -->", "")
 
-                with pathlib.Path.open(file_path, "w") as myfile:
+                with pathlib.Path.open(file_path, "w", encoding="utf-8") as myfile:
                     myfile.write(joined_lines)
 
 
